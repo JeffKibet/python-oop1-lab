15a38ebaad5c547572518c650a4f9d220143ce5b
jeff <jefferykibet@gmail.com>
2026-09-05 10:21:16 +0300

    Implement Coffee class with size and price attributes

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+
+    VALID_SIZES = ["Small", "Medium", "Large"]
+
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+        
