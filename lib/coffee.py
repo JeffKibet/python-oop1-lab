07ac0c9b459df3b51211c474ab5f8c5b3ba04ed7
jeff <jefferykibet@gmail.com>
2026-09-05 10:25:12 +0300

    Add tip method to Coffee class to increase price and provide feedback

--- lib/coffee.py
+++ lib/coffee.py
@@ -18,3 +18,8 @@ class Coffee:
             self._size = size
         else:
             print("size must be Small, Medium, or Large")
+
+
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        self.price += 1
